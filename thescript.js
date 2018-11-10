9d254333598fb34f40c7e8d6200d802af6cb7739
bandy123 <44879887+bandy123@users.noreply.github.com>
2018-11-10 14:44:16 -0400

    Add files via upload

---
+++ thescript.js
@@ -0,0 +1,7 @@
+let sum = 0
+
+document.querySelectorAll('[id^="widget_"], #shipping_price').forEach(tag =>
+  sum += parseFloat(tag.textContent.slice(1))
+)
+
+document.querySelector('#total_price').textContent = `$${Math.round(sum * 100) / 100}`
